9562f40875c745d3a02d10aa5945e5fe3440a222
Timur Alperovich <timuralp@swiftstack.com>
2016-05-25 16:40:49 -0700

    Add requirements to setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as reqs_file:
+    reqs = [req.strip() for req in reqs_file]
+
 setup(name='swift-s3-sync',
       version='0.0.1',
       author='SwiftStack',
       test_suite='nose.collector',
       url='https://github.com/swiftstack/swift-s3-sync',
       packages=['s3_sync'],
+      install_requires = reqs,
       entry_points={
           'console_scripts': [
               'swift-s3-sync = s3_sync.__main__:main'
